841ec4f26aa594df09fc36955fd403c3f6cbbe39
devanshika <devanshika4@gmail.com>
2021-02-25 16:14:57 -0500

    Improved the parser to keep track of all relationships as well as provide information on dataset, dimensions and measures

--- index.js
+++ index.js
@@ -17,114 +17,124 @@ const port = process.env.PORT || "8000";
 const fs = require("fs")
 const rdfParser = require("rdf-parse").default;
 const N3 = require('n3');
-
-
 const store = new N3.Store();
-const quadStream = rdfParser.parse(fs.createReadStream('C:\\Users\\devan\\Downloads\\output_file.ttl'),
-    { contentType: 'text/turtle' });
-store.import(quadStream)
-    .on('end',
-        () => {
-            
-            const allQuads = store.getQuads(null, null, null);
-            ontologyList = [];
-            for (i = 0; i < allQuads.length; i++) {
-                subjectval = allQuads[i].subject.value;
-                objectval = allQuads[i].object.value;
-                if (subjectval.indexOf("http") >= 0) {
-                    pos = subjectval.indexOf("#");
-                    if (pos != -1) {
-                        subjectval = subjectval.substring(0, pos);
-                        subjectval = subjectval.match(/^https?:\/\/[^#?\/]+/)[0];
-                        slash = subjectval.lastIndexOf('/');
-                        subjectval = subjectval.substring(slash + 1);
-                        if (ontologyList.indexOf(subjectval) < 0) {
-                            ontologyList.push(subjectval);
-                        }
-                    }
-                    else {
-                        subjectval = subjectval.match(/^https?:\/\/[^#?\/]+/)[0];
-                        slash = subjectval.lastIndexOf('/');
-                        subjectval = subjectval.substring(slash + 1);
-                        if (ontologyList.indexOf(subjectval) < 0) {
-                            ontologyList.push(subjectval);
-                        }
+
+launch_application = function () {
+    allQuads = store.getQuads(null, null, null);
+    ontologyList = [];
+    joinednodes = {}
+    links = []
+    dimensionList = []
+    measureList = []
+    datasetList = []
+    for (i = 0; i < allQuads.length; i++) {
+        subjectval = allQuads[i].subject.value;
+        objectval = allQuads[i].object.value;
+        predicateval = allQuads[i].predicate.value;
+        if (!(subjectval in joinednodes)) {
+            joinednodes[subjectval] = { "id": subjectval, "relationships": [], "group": "" }
+        }
+        joinednodes[subjectval].relationships.push({ "Predicate": predicateval, "Object": objectval })
+        subjedited = ""
+        if (subjectval.indexOf("http") >= 0) {
+            pos = subjectval.indexOf("#");
+            if (pos >= 0) {
+                if (subjectval.indexOf("dataset-") >= 0) {
+                    datasetName = subjectval.substring(subjectval.lastIndexOf("-") + 1)
+                    if (datasetList.indexOf(datasetName) < 0) {
+                        datasetList.push(datasetName);
                     }
                 }
-                if (objectval.indexOf("http") >= 0) {
-                    pos = objectval.indexOf("#");
-                    if (pos != -1) {
-                        objectval = objectval.substring(0, pos);
-                        objectval = objectval.match(/^https?:\/\/[^#?\/]+/)[0];
-                        slash = objectval.lastIndexOf('/');
-                        objectval = objectval.substring(slash + 1);
-                        if (ontologyList.indexOf(objectval) < 0) {
-                            ontologyList.push(objectval);
-                        }
-                    }
-                    else {
-                        objectval = objectval.match(/^https?:\/\/[^#?\/]+/)[0];
-                        slash = objectval.lastIndexOf('/');
-                        objectval = objectval.substring(slash + 1);
-                        if (ontologyList.indexOf(objectval) < 0) {
-                            ontologyList.push(objectval);
-                        }
-                    }
+                subjedited = subjectval.substring(0, pos);
+                subjedited = subjedited.match(/^https?:\/\/[^#?\/]+/)[0];
+                slash = subjedited.lastIndexOf('/');
+                subjedited = subjedited.substring(slash + 1);
+                if (ontologyList.indexOf(subjedited) < 0) {
+                    ontologyList.push(subjedited);
                 }
             }
-
-            
-            data = { "nodes": [], "links": [], "ontologylist" :ontologyList };
-            nodeList = [];
-            for (i = 0; i < allQuads.length; i++) {
-                subjectval = allQuads[i].subject.value;
-                objectval = allQuads[i].object.value;
-                if (subjectval.indexOf("http") >= 0) {
-                    if (nodeList.indexOf(subjectval) < 0) {
-                        nodeList.push(subjectval);
-                        subject = subjectval.match(/^https?:\/\/[^#?\/]+/)[0];
-                        slash = subject.lastIndexOf('/');
-                        subject = subject.substring(slash + 1);
-                        node = { "id": subjectval, "group": ontologyList.indexOf(subject) };
-                        data.nodes.push(node);
-                    }
-                }
-                if (objectval.indexOf("http") >= 0) {
-                    if (nodeList.indexOf(objectval) < 0) {
-                        nodeList.push(objectval);
-                        object = objectval.match(/^https?:\/\/[^#?\/]+/)[0];
-                        slash = object.lastIndexOf('/');
-                        object = object.substring(slash + 1);
-                        node = { "id": objectval, "group": ontologyList.indexOf(object) };
-                        data.nodes.push(node);
-                    }
-                }
-                if (subjectval.indexOf("http") >= 0 && objectval.indexOf("http") >= 0) {
-                    link = { "source": subjectval, "target": objectval, "value": 1 };
-                    data.links.push(link);
+            else {
+                subjedited = subjectval.match(/^https?:\/\/[^#?\/]+/)[0];
+                slash = subjedited.lastIndexOf('/');
+                subjedited = subjedited.substring(slash + 1);
+                if (ontologyList.indexOf(subjedited) < 0) {
+                    ontologyList.push(subjedited);
                 }
             }
-            
-            /**
-             * Routes Definitions
-             */
+        }
+        joinednodes[subjectval].group = subjedited;
+        if (objectval.indexOf("http") >= 0) {
+            objedited = ""
+            if (!(objectval in joinednodes)) {
+                joinednodes[objectval] = { "id": objectval, "relationships": [], "group": "" }
+            }
+            joinednodes[objectval].relationships.push({ "Predicate": predicateval, "Subject": subjectval })
+            links.push({ "source": subjectval, "target": objectval, "value": 1 });
+            pos = objectval.indexOf("#");
+            if (pos >= 0) {
+                if (predicateval.indexOf("measure") >= 0) //measure
+                {
+                    measureVal = objectval.substring(pos + 1)
+                    if (measureList.indexOf(measureVal) < 0) {
+                        measureList.push(measureVal);
+                    }
+                }
+                if (predicateval.indexOf("dimension") >= 0) //dimension
+                {
+                    dimensionVal = objectval.substring(pos + 1)
+                    if (dimensionList.indexOf(dimensionVal) < 0) {
+                        dimensionList.push(dimensionVal);
+                    }
+                }
+                objedited = objectval.substring(0, pos);
+                objedited = objedited.match(/^https?:\/\/[^#?\/]+/)[0];
+                slash = objedited.lastIndexOf('/');
+                objedited = objedited.substring(slash + 1);
+                if (ontologyList.indexOf(objedited) < 0) {
+                    ontologyList.push(objedited);
+                }
+            }
+            else {
+                objedited = objectval.match(/^https?:\/\/[^#?\/]+/)[0];
+                slash = objedited.lastIndexOf('/');
+                objedited = objedited.substring(slash + 1);
+                if (ontologyList.indexOf(objedited) < 0) {
+                    ontologyList.push(objedited);
+                }
+            }
+            joinednodes[objectval].group = objedited;
+        }
+    }
 
-            app.use(express.static(__dirname + '/Express'));
-             
-            app.get("/", (req, res) => {
-                res.status(200).sendFile(path.join(__dirname+'/Express/index.html'));    
-                      
-            });
-            app.post("/clicked", (req, res) => {
-                return res.status(200).json({
-                    ok: true,
-                    data: data
-                });
-            });
-            /**
-             * Server Activation
-             */
-            app.listen(port, () => {
-                console.log(`Listening to requests on http://localhost:${port}`);
-            });
+    ontologyList.push("")
+    data = { "nodes": [], "links": links, "ontologylist": ontologyList, "datasets": datasetList, "dimensions": dimensionList, "measures": measureList };
+    for (var key in joinednodes) {
+        data.nodes.push(joinednodes[key]);
+    }
+
+    /**
+     * Routes Definitions
+     */
+
+    app.use(express.static(__dirname + '/Express'));
+
+    app.get("/", (req, res) => {
+        res.status(200).sendFile(path.join(__dirname + '/Express/index.html'));
+
+    });
+    app.post("/getRDFData", (req, res) => {
+        return res.status(200).json({
+            ok: true,
+            data: data
         });
+    });
+    /**
+     * Server Activation
+     */
+    app.listen(port, () => {
+        console.log(`Listening to requests on http://localhost:${port}`);
+    });
+}
+
+quadStream = rdfParser.parse(fs.createReadStream(__dirname + '/output_file.ttl'), { contentType: 'text/turtle' });
+store.import(quadStream).on('end', launch_application);
